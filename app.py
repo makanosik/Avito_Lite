58435f41bce942868e55e8bf4af31d905a22d99a
Schef <drslik@yandex.ru>
2024-10-17 12:41:49 +0500

    Tik4 (регистрация c пдтв работает)

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ app.config['MAIL_DEFAULT_SENDER'] = 'makanosik@gmail.com'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'makanosik@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ckdz kfzw whjy uyok'
+app.config['MAIL_USERNAME'] = 'bigxxuser@gmail.com'
+app.config['MAIL_PASSWORD'] = 'trgr xsbg koqo zbwl'
 mail = Mail(app)
 
 
